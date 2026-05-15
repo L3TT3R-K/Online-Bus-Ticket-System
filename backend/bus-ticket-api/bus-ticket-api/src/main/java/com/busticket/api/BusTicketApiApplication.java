9a987b8fd13c8cf84dda643af37279cf5382e644
Nguyễn Thế Khánh <24520795@gm.uit.edu.vn>
2026-05-15 10:56:48 +0700

    cap nhat scheduler

--- backend/bus-ticket-api/bus-ticket-api/src/main/java/com/busticket/api/BusTicketApiApplication.java
+++ backend/bus-ticket-api/bus-ticket-api/src/main/java/com/busticket/api/BusTicketApiApplication.java
@@ -2,10 +2,8 @@ package com.busticket.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class BusTicketApiApplication {
 
     public static void main(String[] args) {
